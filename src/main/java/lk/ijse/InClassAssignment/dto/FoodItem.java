ea0c8cb6aa5fcf5f541ac52fe552f851e4d62ae7
gayanukabulegoda <grbulegoda@gmail.com>
2024-09-01 10:25:56 +0530

    work finalized

---
+++ src/main/java/lk/ijse/InClassAssignment/dto/FoodItem.java
@@ -0,0 +1,13 @@
+package lk.ijse.InClassAssignment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FoodItem {
+    private String name;
+    private String category;
+}
